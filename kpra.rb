5ffed5476d9006e0f364f55d4e4932267d1e1c97
Den Patin <denpatin@gmail.com>
2016-02-29 19:19:17 +0300

    Normal indentation

--- kpra.rb
+++ kpra.rb
@@ -2,61 +2,61 @@ require 'nokogiri'
 require 'httpclient'
 
 def get_content(url)
-	browser = 'Chrome/41.0.2228.0'
-	encoding = 'deflate'
-	http_params = { 'User-Agent' => browser, 'Accept-Encoding' => encoding }
-	client = HTTPClient.new('http://proxy.t-systems.ru:3128')
-	response = client.get url, nil, http_params
-	fail Class.new StandardError, 'Not accessible!' unless response.status == 200
-	Nokogiri::HTML response.body.encode('utf-8')
+  browser = 'Chrome/41.0.2228.0'
+  encoding = 'deflate'
+  http_params = { 'User-Agent' => browser, 'Accept-Encoding' => encoding }
+  client = HTTPClient.new('http://proxy.t-systems.ru:3128')
+  response = client.get url, nil, http_params
+  fail Class.new StandardError, 'Not accessible!' unless response.status == 200
+  Nokogiri::HTML response.body.encode('utf-8')
 end
 
 def get_reviews(id, mood, per_page, mode)
-	url = "http://www.kinopoisk.ru/film/#{id}/ord/rating/status/#{mood}/perpage/#{per_page}"
-	content = get_content(url)
-	unless content.search('.pagesFromTo').first.nil?
-		count = content.search('.pagesFromTo').first.text[/\d+$/].to_i
-		open("#{id}_#{mood}.txt", 'w') { |f|
-			# per_page = count < 10 ? count : content.search('.show select option[last()]').first.text.to_i
-			# review_url = "http://www.kinopoisk.ru/film/#{id}/ord/rating/status/#{mood}/perpage/#{per_page}"
-			# if count > 200
-			page = count / per_page + (count % per_page != 0 ? 1 : 0)
-			if page == 1
-				case mode
-				when 'quick'
-					STDOUT.write "\r#{count} of #{count} reviews extracted"
-					f.puts content.search(".reviewItem table .brand_words p span").text
-				when 'precise'
-					[*1..count].each do |review|
-						STDOUT.write "\r#{review} of #{count} reviews extracted"
-						f.puts content.search(".reviewItem table .brand_words p span")[review-1].text
-					end
-				else
-					puts 'Nonexistent mode!'
-				end
-			else
-				[*1..page].each do |page_review|
-					page_content = get_content("#{url}/page/#{page_review}")
-					reviews_left = count-per_page*(page_review-1) > per_page ? per_page : count % per_page
-					case mode
-					when 'quick'
-						STDOUT.write "\r#{(page_review-1)*per_page+(count % per_page)} of #{count} reviews extracted"
-						f.puts page_content.search(".reviewItem table .brand_words p span").text
-					when 'precise'
-						[*1..reviews_left].each do |review|
-							STDOUT.write "\r#{(page_review-1)*per_page+review} of #{count} reviews extracted"
-							f.puts page_content.search(".reviewItem table .brand_words p span")[review-1].text
-						end
-					else
-						puts 'Nonexistent mode!'
-					end
-				end
-			end
-		}
-		puts
-	else
-		puts 'No reviews!'
-	end
+  url = "http://www.kinopoisk.ru/film/#{id}/ord/rating/status/#{mood}/perpage/#{per_page}"
+  content = get_content(url)
+  unless content.search('.pagesFromTo').first.nil?
+    count = content.search('.pagesFromTo').first.text[/\d+$/].to_i
+    open("#{id}_#{mood}.txt", 'w') { |f|
+      # per_page = count < 10 ? count : content.search('.show select option[last()]').first.text.to_i
+      # review_url = "http://www.kinopoisk.ru/film/#{id}/ord/rating/status/#{mood}/perpage/#{per_page}"
+      # if count > 200
+      page = count / per_page + (count % per_page != 0 ? 1 : 0)
+      if page == 1
+        case mode
+        when 'quick'
+          STDOUT.write "\r#{count} of #{count} reviews extracted"
+          f.puts content.search(".reviewItem table .brand_words p span").text
+        when 'precise'
+          [*1..count].each do |review|
+            STDOUT.write "\r#{review} of #{count} reviews extracted"
+            f.puts content.search(".reviewItem table .brand_words p span")[review-1].text
+          end
+        else
+          puts 'Nonexistent mode!'
+        end
+      else
+        [*1..page].each do |page_review|
+          page_content = get_content("#{url}/page/#{page_review}")
+          reviews_left = count-per_page*(page_review-1) > per_page ? per_page : count % per_page
+          case mode
+          when 'quick'
+            STDOUT.write "\r#{(page_review-1)*per_page+(count % per_page)} of #{count} reviews extracted"
+            f.puts page_content.search(".reviewItem table .brand_words p span").text
+          when 'precise'
+            [*1..reviews_left].each do |review|
+              STDOUT.write "\r#{(page_review-1)*per_page+review} of #{count} reviews extracted"
+              f.puts page_content.search(".reviewItem table .brand_words p span")[review-1].text
+            end
+          else
+            puts 'Nonexistent mode!'
+          end
+        end
+      end
+    }
+    puts
+  else
+    puts 'No reviews!'
+  end
 end
 
 # p pages = count / per_page + (count % per_page != 0 ? 1 : 0)
--- ym.rb
+++ ym.rb
@@ -4,7 +4,7 @@ require 'yandex_mystem'
 
 result = Array.new
 File.open(ARGV[0]).read.scan(/[a-zа-яеё-]{20,}/i).each do |word|
-	result << YandexMystem::Simple.stem(word).values
+  result << YandexMystem::Simple.stem(word).values
 end
 
 result.flatten.inject(Hash.new(0)){ |f, w| f[w] += 1; f }.to_a.sort_by{ |x| -x[1] }[0,20].each{ |w, f| puts "%-30s%d" % [w, f] }
